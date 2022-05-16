127f8de0e792afc780d268124d40a6e6b8449f9d
tristan.britt@mail.mcgill.ca <tristan.britt@mail.mcgill.ca>
2022-05-16 11:08:31 -0400

    removed circular import

--- pyQE/QEutils.py
+++ pyQE/QEutils.py
@@ -16,8 +16,32 @@ from math import isclose, sqrt
 import numpy as np
 import os
 import re
-from .phononutils import car_red
 
+
+def red_car(red,lat):
+    """
+    Convert reduced coordinates to cartesian
+    """
+    return np.array([coord[0]*lat[0]+coord[1]*lat[1]+coord[2]*lat[2] for coord in red])
+
+def car_red(car,lat):
+    """
+    Convert cartesian coordinates to reduced
+    """
+    return np.array([np.linalg.solve(np.array(lat).T,coord) for coord in car])
+
+def rec_lat(lat):
+    """
+    Calculate the reciprocal lattice vectors
+    """
+    a1,a2,a3 = np.array(lat)
+    v = np.dot(a1,np.cross(a2,a3))
+    b1 = np.cross(a2,a3)/v
+    b2 = np.cross(a3,a1)/v
+    b3 = np.cross(a1,a2)/v
+    return np.array([b1,b2,b3])
+
+    
 e1, e2, e3 = np.eye(3) #standard basis
 def change_of_basis(basis1, basis2=(e1, e2, e3)):
     """
--- pyQE/phononutils.py
+++ pyQE/phononutils.py
@@ -567,28 +567,7 @@ for Z, symbol in enumerate(chemical_symbols):
 
 import numpy as np
 
-def red_car(red,lat):
-    """
-    Convert reduced coordinates to cartesian
-    """
-    return np.array([coord[0]*lat[0]+coord[1]*lat[1]+coord[2]*lat[2] for coord in red])
 
-def car_red(car,lat):
-    """
-    Convert cartesian coordinates to reduced
-    """
-    return np.array([np.linalg.solve(np.array(lat).T,coord) for coord in car])
-
-def rec_lat(lat):
-    """
-    Calculate the reciprocal lattice vectors
-    """
-    a1,a2,a3 = np.array(lat)
-    v = np.dot(a1,np.cross(a2,a3))
-    b1 = np.cross(a2,a3)/v
-    b2 = np.cross(a3,a1)/v
-    b3 = np.cross(a1,a2)/v
-    return np.array([b1,b2,b3])
 
 
 def estimate_band_connection(prev_eigvecs, eigvecs, prev_band_order):
